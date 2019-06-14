73b032b6f6d7a1ff51d2b911141b368ad11221a7
railken <railken@protonmail.ch>
2019-06-14 02:04:44 +0200

    update mapping

--- src/Console/Commands/DataViewSeedCommand.php
+++ src/Console/Commands/DataViewSeedCommand.php
@@ -106,7 +106,7 @@ class DataViewSeedCommand extends Command
 
     public function getRelationsByClassModel(string $classModel)
     {
-        return Collection::make(Mapper::relations($classModel))->map(function ($relation, $key) {
+        return Collection::make(app('eloquent.mapper')->getFinder()->relations($classModel))->map(function ($relation, $key) {
             return array_merge($relation->toArray(), [
                 'key'  => $key,
                 'data' => app('amethyst')->getNameDataByModel($relation->model),
