74750417d0fb16fb3f92590cf71ade9999307d97
railken <railken@protonmail.ch>
2019-07-14 13:05:00 +0200

    phpdoc

--- src/Managers/DataViewManager.php
+++ src/Managers/DataViewManager.php
@@ -5,6 +5,14 @@ namespace Amethyst\Managers;
 use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
+/**
+ * @method \Amethyst\Models\DataView newEntity()
+ * @method \Amethyst\Schemas\DataViewSchema getSchema()
+ * @method \Amethyst\Repositories\DataViewRepository getRepository()
+ * @method \Amethyst\Serializers\DataViewSerializer getSerializer()
+ * @method \Amethyst\Validators\DataViewValidator getValidator()
+ * @method \Amethyst\Authorizers\DataViewAuthorizer getAuthorizer()
+ */
 class DataViewManager extends Manager
 {
     use ConfigurableManager;
