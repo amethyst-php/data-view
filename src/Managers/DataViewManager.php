d2302a60bffccb4dcd4cb9a67968430de77b6f24
railken <railken@protonmail.ch>
2019-07-14 13:08:07 +0200

    fix style

--- src/Managers/DataViewManager.php
+++ src/Managers/DataViewManager.php
@@ -6,12 +6,12 @@ use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
 /**
- * @method \Amethyst\Models\DataView newEntity()
- * @method \Amethyst\Schemas\DataViewSchema getSchema()
+ * @method \Amethyst\Models\DataView                 newEntity()
+ * @method \Amethyst\Schemas\DataViewSchema          getSchema()
  * @method \Amethyst\Repositories\DataViewRepository getRepository()
- * @method \Amethyst\Serializers\DataViewSerializer getSerializer()
- * @method \Amethyst\Validators\DataViewValidator getValidator()
- * @method \Amethyst\Authorizers\DataViewAuthorizer getAuthorizer()
+ * @method \Amethyst\Serializers\DataViewSerializer  getSerializer()
+ * @method \Amethyst\Validators\DataViewValidator    getValidator()
+ * @method \Amethyst\Authorizers\DataViewAuthorizer  getAuthorizer()
  */
 class DataViewManager extends Manager
 {
